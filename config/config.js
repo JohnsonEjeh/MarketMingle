70438c033f2a3291cfb3312c8c52c7cf185a53f6
bexxin <109035845+bexxin@users.noreply.github.com>
2023-12-06 18:09:16 -0500

    functioning server, try to add in all of frontend

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ const config = {
  env: process.env.NODE_ENV || 'development', 
  port: process.env.PORT || 3000,
  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
- mongoUri: process.env.MONGODB_URI || "mongodb+srv://johnsonejeh02:bxdvz8JL28ST9dkD@marketmingle.cux9zuj.mongodb.net/?retryWrites=true&w=majority"||
+ mongoUri: process.env.MONGODB_URI || "mongodb+srv://marketmingle:6mODVrpJ96kUET5w@cluster0.dlwzq1d.mongodb.net/test?retryWrites=true&w=majority"||
 process.env.MONGO_HOST ||
  'mongodb://' + (process.env.IP || 'localhost') + ':' + 
 (process.env.MONGO_PORT || '27017') +
