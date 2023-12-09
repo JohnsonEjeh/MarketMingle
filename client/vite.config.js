5cfcd089975e577f1e940b5a523c4dbdff4dd059
Ejeh <ejehsax@gmail.com>
2023-12-09 15:06:18 -0500

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "https://market-mingle-api.onrender.com",
+        target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
     },
